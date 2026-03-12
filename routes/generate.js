9bfef5038725b8876f92a54a182fc1dca810d301
Yashaswini <yashaswinikc2006@gmail.com>
2026-03-12 16:40:00 +0530

    Added responsive layout support

--- routes/generate.js
+++ routes/generate.js
@@ -91,6 +91,7 @@ Example manifest:
       ]
     });
      // console.log('Raw response:', completion.choices[0].message.content);
+     // console.log('Parsed files:', files);
     const files = JSON.parse(
       completion.choices[0].message.content
     );

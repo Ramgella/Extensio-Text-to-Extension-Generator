049ce90fd0e0ea8d1f2701d09b96f42392ca6348
Ram Gella <gellaram2006@gmail.com>
2026-04-23 13:20:00 +0530

    Optimized extension generation workflow

--- routes/generate.js
+++ routes/generate.js
@@ -91,7 +91,7 @@ Example manifest:
       ]
     });
      // console.log('Raw response:', completion.choices[0].message.content);
-     // console.log('Parsed files:', files);aae
+     // console.log('Parsed files:', files);aaeww
     const files = JSON.parse(
       completion.choices[0].message.content
     );
